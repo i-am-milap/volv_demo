c8b01fa0a093899105a5c523a21dbf3cb265bf85
Milap Shah <milap.shah@rocketmail.com>
2021-09-02 14:10:49 +0530

    article image change and gradient on home page added

--- lib/images/images.dart
+++ lib/images/images.dart
@@ -8,5 +8,10 @@ class Images {
   static const intro2 = "assets/images/intro-slider/intro2.png";
   static const intro3 = "assets/images/intro-slider/intro3.png";
 
+  static const homePlaceholder = "assets/images/home-placeholder.jpg";
   static const homeBg = "assets/images/home-bg.jpg";
+  static const homeBg1 = "assets/images/home-bg1.jpg";
+  static const homeBg2 = "assets/images/home-bg2.jpg";
+  static const homeBg3 = "assets/images/home-bg3.jpg";
+  static const homeBg4 = "assets/images/home-bg4.jpg";
 }
--- lib/pagees/home.dart
+++ lib/pagees/home.dart
@@ -2,8 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:volv_demo/images/images.dart';
 import 'package:volv_demo/theme/appColors.dart';
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final List _postImages = [
+    Images.homeBg,
+    Images.homeBg1,
+    Images.homeBg2,
+    Images.homeBg3,
+    Images.homeBg4,
+  ];
+
+  late AssetImage _postImage;
+
+  @override
+  void initState() {
+    super.initState();
+    _postImage = AssetImage(_postImages[0]);
+  }
+
+  changePostImage(int index) {
+    setState(() {
+      _postImage = AssetImage(
+        _postImages[index],
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,17 +39,32 @@ class Home extends StatelessWidget {
       body: Stack(
         fit: StackFit.passthrough,
         children: [
-          Container(
+          FadeInImage(
             height: MediaQuery.of(context).size.height,
-            padding: const EdgeInsets.symmetric(horizontal: 24.0),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(Images.homeBg),
-                fit: BoxFit.cover,
+            fit: BoxFit.cover,
+            image: _postImage,
+            placeholder: AssetImage(Images.homePlaceholder),
+            fadeOutDuration: new Duration(milliseconds: 150),
+            fadeOutCurve: Curves.decelerate,
+            alignment: Alignment.center,
+            repeat: ImageRepeat.noRepeat,
+          ),
+          Positioned(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.transparent,
+                    Colors.white,
+                    Colors.white,
+                    Colors.white,
+                  ],
+                  stops: [0.42, 0.6, 0.7, 1.0],
+                ),
               ),
-            ),
-            child: Center(
-              child: Text("hello"),
             ),
           ),
           Positioned(
@@ -51,88 +93,105 @@ class Home extends StatelessWidget {
               decoration: BoxDecoration(
                 color: AppColors.white,
                 borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.26),
+                    spreadRadius: 4,
+                    blurRadius: 5,
+                    offset: Offset(0, 1),
+                  ),
+                ],
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing",
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w900,
-                      letterSpacing: 1.1,
-                    ),
-                  ),
-                  SizedBox(height: 16.0),
-                  Text(
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    style: TextStyle(
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.w400,
-                      letterSpacing: 1.1,
-                      height: 1.4,
-                    ),
-                  ),
-                  SizedBox(height: 16.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              child: PageView.builder(
+                itemCount: 5,
+                scrollDirection: Axis.vertical,
+                onPageChanged: (int index) {
+                  this.changePostImage(index);
+                },
+                itemBuilder: (BuildContext context, int index) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
+                      Text(
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w900,
+                          letterSpacing: 1.1,
+                        ),
+                      ),
+                      SizedBox(height: 16.0),
+                      Text(
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 1.1,
+                          height: 1.4,
+                        ),
+                      ),
+                      SizedBox(height: 16.0),
                       Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Icon(
-                            Icons.schedule_outlined,
-                            size: 18.0,
-                            color: AppColors.greyColor,
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.schedule_outlined,
+                                size: 18.0,
+                                color: AppColors.greyColor,
+                              ),
+                              SizedBox(width: 4.0),
+                              Text(
+                                "Today",
+                                style: TextStyle(
+                                  fontSize: 14.0,
+                                  color: AppColors.greyColor,
+                                ),
+                              ),
+                            ],
                           ),
-                          SizedBox(width: 4.0),
-                          Text(
-                            "Today",
-                            style: TextStyle(
-                              fontSize: 14.0,
-                              color: AppColors.greyColor,
+                          TextButton(
+                            child: Text(
+                              "CHECK IT OUT",
+                              style: TextStyle(
+                                fontSize: 12.0,
+                                color: AppColors.secondaryColor,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 1.1,
+                              ),
+                            ),
+                            onPressed: () {},
+                            style: TextButton.styleFrom(),
+                          ),
+                          InkWell(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Share",
+                                  style: TextStyle(
+                                    fontSize: 14.0,
+                                    color: AppColors.greyColor,
+                                  ),
+                                ),
+                                SizedBox(width: 4.0),
+                                Icon(
+                                  Icons.ios_share_rounded,
+                                  size: 18.0,
+                                  color: AppColors.greyColor,
+                                ),
+                              ],
                             ),
                           ),
                         ],
                       ),
-                      TextButton(
-                        child: Text(
-                          "CHECK IT OUT",
-                          style: TextStyle(
-                            fontSize: 12.0,
-                            color: AppColors.secondaryColor,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 1.1,
-                          ),
-                        ),
-                        onPressed: () {},
-                        style: TextButton.styleFrom(),
-                      ),
-                      InkWell(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(
-                              "Share",
-                              style: TextStyle(
-                                fontSize: 14.0,
-                                color: AppColors.greyColor,
-                              ),
-                            ),
-                            SizedBox(width: 4.0),
-                            Icon(
-                              Icons.ios_share_rounded,
-                              size: 18.0,
-                              color: AppColors.greyColor,
-                            ),
-                          ],
-                        ),
-                      ),
                     ],
-                  ),
-                ],
+                  );
+                },
               ),
             ),
           ),
